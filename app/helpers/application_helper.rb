d51b4eeed17cf344e91feb8c1b4f5edcb5b571fd
AjuntaLEE <ajunta.lee@mail.com>
2013-10-09 00:13:05 +0200

    added full titele helper to the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails TWR-LFBI Almanac App v 0.0.1 (alpha)"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
