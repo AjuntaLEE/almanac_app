80704fff59f55616e9a7040ba7b36b287786bdb5
AjuntaLEE <ajunta.lee@mail.com>
2013-10-13 02:16:56 +0200

    make a basic user model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,18 +8,19 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
   def greet_user()
-	time = Time.new
-	if time.hour > 14
-		"Bonsoir"
-	else
-		"Bonjour"
-	end
+  	time = Time.new
+  	if time.hour > 14
+  		"Bonsoir"
+  	else
+  		"Bonjour"
+  	end
   end
+
   def helper_date()
   	time = Time.new.utc
 	"#{["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"][time.wday]} #{time.day} #{["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre",
 	"Octobre","Novembre","Décembre"][time.month-1]} #{time.year} #{time.hour}:#{time.min} Z."
   end
-
 end
---
+++ app/models/user.rb
@@ -0,0 +1,59 @@
+require 'digest'
+class User < ActiveRecord::Base
+	attr_accessor :password, :password_confirmation
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_validation :encrypt_password
+
+	validates :name, presence: true, length: {maximum: 45}
+	
+	validates :email, presence: true, 
+					format:  { with: VALID_EMAIL_REGEX },
+					uniqueness: { case_sensitive: false }
+	validates :password, :presence => true,
+                       :length => { :within => 6..40 }
+  validates :password_digest, :presence   => true
+
+  before_save { self.email = email.downcase }
+
+	def self.authenticate(email, submitted_password)
+  	user = find_by_email(email)
+  	return nil  if user.nil?
+  	return user if user.has_password?(submitted_password)
+	end
+
+	# Return true if the user's password matches the submitted password.
+	def has_password?(submitted_password)
+	# Compare password_digest with the encrypted version of
+	# submitted_password.
+		self.password_digest == encrypt(submitted_password)
+	end
+
+  private
+
+  def encrypt_password
+    puts "encryption password_digest "
+    self.salt = make_salt unless has_password?(password)
+    if secure_hash("#{self.password}") == secure_hash("#{password_confirmation}")
+      self.password_digest = encrypt(password)
+    else 
+      self.password_digest = ""
+    end
+  end
+
+  def encrypt(string)
+    puts "encryption core"
+    return secure_hash("#{self.salt}--#{string}")
+  end
+
+  def make_salt
+    puts "salt generation"
+    return secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    puts "generating secure hash"
+    return Digest::SHA2.hexdigest(string)
+  end
+
+end
