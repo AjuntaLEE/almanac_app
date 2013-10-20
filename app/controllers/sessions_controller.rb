7cdc968c628942f6ccd61673e8224f5a789fb693
AjuntaLEE <ajunta.lee@mail.com>
2013-10-20 23:41:16 +0200

    Finish  user edit, update, inde, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   	if user && user.has_password?(params[:session][:password])
   	  flash[:success] = "#{greet_user} #{user.name}, votre session est désormais active n'oubliez pas de détruire cette session dès que vous n'en avez plus besoin"
       sign_in user
-      redirect_to user
+      redirect_back_or user
   	else
       flash.now[:error] = "Combinaison 'E-mail' <-> 'Mot de passe' inconnue" # Not quite right!
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+
+  def index
+    @users = User.paginate(page: params[:page],:per_page => 25)
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -18,11 +27,48 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Votre profil à été édité avec succés"
+      redirect_to @user
+    else
+      flash.now[:error] = "Votre profil n'a pas été édité car le formulaire en l'état actuel est incomplet ou certains champs sont invalides"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Utilisateur éffacé"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
